7f2ec5216ff17af00386042912a38971c019de03
John Connor <jckusho@gmail.com>
2023-08-01 15:21:05 -0400

    Improved much and removed some dead weight
    It's a bit touchy and unstable. Not sure why it doesn't work every time, but if you really want a screenshot you can just move around and keep spamming the screenshot button until you get a good one. Pretty sure PAC parts won't be rendered in it though.

--- lua/weapons/gmod_camera_xray.lua
+++ lua/weapons/gmod_camera_xray.lua
@@ -22,28 +22,54 @@ if CLIENT then
 	XRays.path = "xray_camera/"
 	XRays.next = nil
 
+	file.CreateDir(XRays.path)
+
 	concommand.Add("png_xray", function(_, _, _, argStr)
 		XRays.next = #argStr > 0 and argStr or util.DateStamp()
 	end)
 
-	hook.Add("PreDrawOpaqueRenderables", "XRayCamera.Capture.PreDraw", function()
-		if XRays.next then
-			render.ClearDepth()
+	local msg_orange = Color(255, 100, 0)
+	local msg_yellow = Color(255, 255, 0)
+
+	--- Alias for capturing an image, writing it and logging it
+	--- in the console.
+	---@param name string? The file name the capture will be saved to
+	local function CaptureImage(name)
+		local path = XRays.path .. name .. ".png"
+		local cap = render.Capture({
+			format = "png",
+			x = 0,
+			y = 0,
+			w = ScrW(),
+			h = ScrH()
+		})
+
+		file.Write(path, cap)
+		MsgC(msg_orange, "[X-ray Camera] ", color_white, "Image captured and saved to ", msg_yellow, "\"" .. path .. "\" (" .. string.NiceSize(file.Size(path, "DATA")) .. ")")
+	end
+
+	local lastrun
+
+	hook.Add("PreDrawOpaqueRenderables", "XRayCamera.Capture.PreDraw", function(isDrawingDepth, _, isDraw3DSkybox)
+		if isDrawingDepth or isDraw3DSkybox then
+			return true
+		elseif XRays.next and lastrun ~= CurTime() then
+			render.SetWriteDepthToDestAlpha(false)
 			render.Clear(0, 0, 0, 0)
+			lastrun = CurTime()
 		end
 	end)
 
-	hook.Add("PostRender", "XRayCamera.Capture.PostRender", function()
+	hook.Add("PostDrawOpaqueRenderables", "XRayCamera.Capture.PostDraw", function(_, _, _)
 		if XRays.next then
-			file.Write(XRays.path .. XRays.next .. ".png", render.Capture({
-				format = "png",
-				x = 0,
-				y = 0,
-				w = ScrW(),
-				h = ScrH()
-			}))
+			render.SetWriteDepthToDestAlpha(true)
+			CaptureImage(XRays.next)
 			XRays.next = nil
 		end
+	end)
+
+	hook.Add("PreDrawSkyBox", "XRayCamera.Capture.DisableSkybox", function()
+		if XRays.next then return true end
 	end)
 end
 
@@ -53,7 +79,7 @@ end
 
 function SWEP:PrimaryAttack()
 	-- Say cheese! Your vision will return shortly.
-	self:DoShootEffects()
+	self:DoShootEffect()
 
 	if not game.SinglePlayer() and SERVER then return end
 	if CLIENT and not IsFirstTimePredicted() then return end
